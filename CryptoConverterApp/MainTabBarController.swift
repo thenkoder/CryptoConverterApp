77dda49b7b470afe204a6572f5db0ce49433f8be
Vladimir Fibe <motiw@me.com>
2019-11-16 12:54:05 +0600

    Создал MainTabBarController

---
+++ CryptoConverterApp/MainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  CryptoConverterApp
+//
+//  Created by Владимир Файб on 16.11.2019.
+//  Copyright © 2019 Другов Родион. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    viewControllers = [
+      generateViewController(rootViewController: ViewController(), image: "arrow.2.circlepath.circle", title: "Convert")
+    ]
+  }
+  private func generateViewController(rootViewController: UIViewController, image: String, title: String) -> UIViewController {
+    
+    let navigationVC = UINavigationController(rootViewController: rootViewController)
+    navigationVC.tabBarItem.title = title
+    navigationVC.tabBarItem.image = UIImage(systemName: image)
+    rootViewController.navigationItem.title = title
+    navigationVC.navigationBar.prefersLargeTitles = true
+    
+    return navigationVC
+  }
+}
